6217eed9ae022b51383f6b1c01ce103e3b7266da
laukik86 <laukikpadgaonkar0@gmail.com>
2025-12-26 20:31:10 +0530

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express =require("express");
+const mongoose=require("mongoose");
+const cors = require("cors");
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("Connected to MongoDB"))
+    .catch((err)=>console.log("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+    res.send("backend is running" );
+});
+const PORT=process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 

05683938008c2a96ec0659fc5c9d89b471d3dcb3
laukik86 <116993291+laukik86@users.noreply.github.com>
2025-12-29 22:57:30 +0530

    Change API base URL environment variable

--- frontend/src/api/car.js
+++ frontend/src/api/car.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export const getAvailableCars = async({location, startDate,endDate})=>{
     const query = new URLSearchParams({
@@ -13,5 +13,6 @@ export const getAvailableCars = async({location, startDate,endDate})=>{
     }
     return res.json();
 };
+
 
 //api calls idhar rakhhe

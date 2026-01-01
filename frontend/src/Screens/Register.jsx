1a2a9def272c971e5b7cd39a48e274276c6eb744
AtharvaPatil86 <146982838+AtharvaPatil86@users.noreply.github.com>
2026-01-01 22:27:30 +0530

    Fix API URL formatting in Register component

--- frontend/src/Screens/Register.jsx
+++ frontend/src/Screens/Register.jsx
@@ -26,7 +26,7 @@ function Register() {
     }
     
     try{
-        const response = await fetch(`import.meta.env.VITE_API_URL/api/auth/register`, {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
